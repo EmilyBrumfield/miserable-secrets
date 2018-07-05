21567f09d73d78a54942eeec01f852caf1f23446
EmilyBrumfield <belac54@hotmail.com>
2018-07-04 23:22:20 -0400

    make more charts

--- charts.js
+++ charts.js
@@ -25,4 +25,58 @@ charts.region = [
     "Edge of the Wastes - In the Foothills", //index 21
     "Edge of the Wastes - Thin Air", //index 22
     "Edge of the Wastes - Mountain Fastness" //index 23
+];
+
+charts.theme = [ 
+    "Market - Caravansary", //index 0
+    "Market - Seasonal Market", //index 1
+    "Market - Bazaar", //index 2
+    "Market - Illicit Trade", //index 3
+    "Agriculture - Herding", //index 4
+    "Agriculture - Orchards", //index 5
+    "Agriculture - Gardens", //index 6
+    "Agriculture - Psychedelics", //index 7
+    "Crossroads - Place of Pilgrimage", //index 8
+    "Crossroads - Contested Borderland", //index 9
+    "Crossroads - Refugee Crisis", //index 10
+    "Crossroads - Smuggler’s Den", //index 11
+    "Craft - Carpentry", //index 12
+    "Craft - Stone-cutting", //index 13
+    "Craft - Brewing", //index 14
+    "Craft - Weaving", //index 15
+    "Industry - Slaughterhouses", //index 16
+    "Industry - Smelting", //index 17
+    "Industry - Lumber", //index 18
+    "Industry - Mining", //index 19
+    "Seat of Power - Noble", //index 20
+    "Seat of Power - Church", //index 21
+    "Seat of Power - Warlord", //index 22
+    "Seat of Power - Criminal" //index 23
+]
+
+charts.troubles = [ 
+    "A monster native to the region stalks the outlying cottages and farms", //index 0
+    "A mysterious explosion destroys an otherwise innocuous building", //index 1
+    "Marauders threaten to burn the town unless it complies with their demands", //index 2
+    "A highly addictive drug is given to local youth, with the promise of more if they comply with some simple demands", //index 3
+    "A sinkhole opens suddenly, consuming several cottages, and the people within", //index 4
+    "A local’s past as a mercenary solider catches up with them when their former comrades arrive and demand the local leave with them", //index 5
+    "A monster of unknown type and foreign nature stalks the town and none know its name or how to stop it", //index 6
+    "A shady character has opened a gambling den, and a surprising number of locals find themselves as regular patrons, despite many being strongly averse to games of chance and wagers. When their losses mount, the shady character offers a deal to help clear those debts.", //index 7
+    "During renovations of the Mayor’s Court, the bones of a ritually sacrificed youth are found packed into the floorboards beneath the judge’s stand, surrounded by the symbols of daemonic worship", //index 8
+    "The crows have begun hanging about, and muttering about the great feast to come", //index 9
+    "A rash of poisoned wells has left several families terribly ill, and a few old folks dead.", //index 10
+    "An ominous cloud hangs over the town, and lighting stabs downwards, though only seems to threaten members of a single clan", //index 11
+    "Monsters normally too stupid or animalist to work together have begun to attack the town as an organized force, with surprising tactical acumen", //index 12
+    "Major local industry or trade collapses, plunging the town into potential ruin", //index 13
+    "Fire ravages the local church, leaving it a burnt-out shell and destroying everything within.", //index 14
+    "A zealot priest militant has come to town to persecute witches, and seems to have no trouble finding targets they deem infernal enough to harass", //index 15
+    "A popular child has run afoul of Wolves while playout outside of town, and they howl louder and creep closer every night until the child is given over to them, or somebody intervenes somehow.", //index 16
+    "A genealogical researcher finds evidence that the first family of the town are illegitimate heirs to the family name and fortunes.", //index 17
+    "A monster demands to be admitted to the town as a citizen, and offers to share its wealth and power to help the town. The price it asks seems so small.", //index 18
+    "Children are snatched in the night, and last night the child of somebody very powerful was taken", //index 19
+    "A harbinger is planting Greenseed around the town", //index 20
+    "A betrothed youth has vanished on their wedding night", //index 21
+    "The Mayor is dead, killed by a single stab to the heart.", //index 22
+    "A foreign noble has arrived in town for mysterious reasons. They and their people are unfailingly polite, but many of the local youth have come down with acute anemia." //index 23
 ]
