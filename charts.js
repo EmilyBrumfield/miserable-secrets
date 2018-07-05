c89a7652eb69b961591672f22c0273492151ed95
EmilyBrumfield <belac54@hotmail.com>
2018-07-04 23:17:05 -0400

    revise chart

--- charts.js
+++ charts.js
@@ -1,52 +1,28 @@
 let charts = {}
 
 charts.region = [ 
-    "Under Green Shadow", //index 0
-    "By the Waters", //index 1
-    "Skeleton City", //index 2
-    "Big Sky Plains", //index 3
-    "Amongst the Crags", //index 4
-    "Edge of the Wastes", //index 5
-];
-
-charts.regionvariation = [
-    //"Under Green Shadow", index 0
-    [
-        "Scent of Summer", //index 0
-        "Edge of the Green", //index 1
-        "Overgrown", //index 2
-        "Bunkertown" //index 3
-    ]
-    //"By the Waters", index 1
-    [
-        "Riverport", //index 0
-        "Seaport", //index 1
-        "In the Marsh", //index 2
-        "Floodtown" //index 3
-    ]
-    //"Skeleton City", index 2
-    [
-        "Tower Town", //index 0
-        "Scavtown", //index 1
-        "Feral Parkland", //index 2
-        "Tunneltown" //index 3
-    ]
-    //"Big Sky Plains", index 3
-    [        
-        "Season’s Nomad Camp", //index 0
-        "Waves of Grain", //index 1
-        "Turbine Farm", //index 2
-        "Badlands" //index 3        
-    ]
-    //"Amongst the Crags", index 4
-    [
-
-    ]
-    //"Edge of the Wastes", index 5
-    [
-        "Shadowed Valley", //index 0
-        "In the Foothills", //index 1
-        "Thin Air", //index 2
-        "Mountain Fastness" //index 3
-    ]
+    "Under Green Shadow - Scent of Summer", //index 0
+    "Under Green Shadow - Edge of the Green", //index 1
+    "Under Green Shadow - Overgrown", //index 2
+    "Under Green Shadow - Bunkertown", //index 3
+    "By the Waters - Riverport", //index 4
+    "By the Waters - Seaport", //index 5
+    "By the Waters - In the Marsh", //index 6
+    "By the Waters - Floodtown", //index 7
+    "Skeleton City - Tower Town", //index 8
+    "Skeleton City - Scavtown", //index 9
+    "Skeleton City - Feral Parkland", //index 10
+    "Skeleton City - Tunneltown", //index 11
+    "Big Sky Plains - Season’s Nomad Camp", //index 12
+    "Big Sky Plains - Waves of Grain", //index 13
+    "Big Sky Plains - Turbine Farm", //index 14
+    "Big Sky Plains - Badlands", //index 15
+    "Among The Crags - Shadowed Valley", //index 16
+    "Among The Crags - In The Foothills", //index 17
+    "Among The Crags - Thin Air", //index 18
+    "Among The Crags - Mountain Fastness", //index 19
+    "Edge of the Wastes - Shadowed Valley", //index 20
+    "Edge of the Wastes - In the Foothills", //index 21
+    "Edge of the Wastes - Thin Air", //index 22
+    "Edge of the Wastes - Mountain Fastness" //index 23
 ]
